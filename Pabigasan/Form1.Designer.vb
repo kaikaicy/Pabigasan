7f83678cc6c016d0b46a87ec7b5276d894027a61
kaikaicy <kikaybenalla5@gmail.com>
2025-01-03 18:20:27 +0800

    login

--- Pabigasan/Form1.Designer.vb
+++ Pabigasan/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class login_page
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,99 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(login_page))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.username_txtb = New System.Windows.Forms.TextBox()
+        Me.pwd_txtb = New System.Windows.Forms.TextBox()
+        Me.login_btn = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ErrorImage = Nothing
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(526, 67)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(299, 278)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(398, 318)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 27)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(398, 362)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(108, 27)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'username_txtb
+        '
+        Me.username_txtb.Font = New System.Drawing.Font("Cascadia Mono", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.username_txtb.Location = New System.Drawing.Point(526, 321)
+        Me.username_txtb.Name = "username_txtb"
+        Me.username_txtb.Size = New System.Drawing.Size(209, 28)
+        Me.username_txtb.TabIndex = 3
+        '
+        'pwd_txtb
+        '
+        Me.pwd_txtb.Font = New System.Drawing.Font("Cascadia Mono", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pwd_txtb.Location = New System.Drawing.Point(526, 362)
+        Me.pwd_txtb.Name = "pwd_txtb"
+        Me.pwd_txtb.Size = New System.Drawing.Size(209, 28)
+        Me.pwd_txtb.TabIndex = 4
+        '
+        'login_btn
+        '
+        Me.login_btn.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(133, Byte), Integer), CType(CType(133, Byte), Integer))
+        Me.login_btn.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.login_btn.ForeColor = System.Drawing.SystemColors.Control
+        Me.login_btn.Location = New System.Drawing.Point(621, 410)
+        Me.login_btn.Name = "login_btn"
+        Me.login_btn.Size = New System.Drawing.Size(114, 45)
+        Me.login_btn.TabIndex = 5
+        Me.login_btn.Text = "Login"
+        Me.login_btn.UseVisualStyleBackColor = False
+        '
+        'login_page
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(152, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1258, 776)
+        Me.Controls.Add(Me.login_btn)
+        Me.Controls.Add(Me.pwd_txtb)
+        Me.Controls.Add(Me.username_txtb)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "login_page"
+        Me.Text = "Pabigasan"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents username_txtb As TextBox
+    Friend WithEvents pwd_txtb As TextBox
+    Friend WithEvents login_btn As Button
 End Class
--- Pabigasan/My Project/Application.Designer.vb
+++ Pabigasan/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Pabigasan.Form1
+            Me.MainForm = Global.Pabigasan.login_page
         End Sub
     End Class
 End Namespace
